10840a792a71a4e4a9f6c6ebfc0e68e8812458ce
emmathedeveloper2 <emmadavid063@gmail.com>
2025-02-20 23:01:50 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import checkForAppUpdates from "./lib/updater";
+import checkForAppUpdates from "@lib/updater";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
